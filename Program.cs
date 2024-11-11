f44fbd342108950ec00232efd7825f9d60b73d30
quinc <quinc@QUINCYPC>
2024-11-11 16:02:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment_2._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         //VEHICLES
+         //AUTOMOBILES
+         //CAR--TRUCK--MOTORCYCLE
+         //FORD--RAM--YAMAHA
+         //
+        }
+    }
+}
